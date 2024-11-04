9f664bb30891254c2d0864025013cc0a27a42294
Zait Martinez <zmtzz1411@gmail.com>
2024-11-04 00:05:30 -0600

    Removed brief statements in headerfile and added to c file.

--- shell_commands.c
+++ shell_commands.c
@@ -1,10 +1,16 @@
 #include "shell_commands.h"
+#include "shell_auxiliary.h"
 
-void yield(void)
-{
-    // Empty function called to yield the processor
-}
+// void yield(void)
+// {
+//     // Empty function called to yield the processor
+// }
 
+/**
+ * @brief 
+ * Reboots the microcontroller. Will be implemenented 
+ * as part of the mini project. 
+ */
 void reboot(void)
 {
     /*
@@ -20,17 +26,30 @@ void reboot(void)
     NVIC_APINT_R = NVIC_APINT_VECTKEY | NVIC_APINT_SYSRESETREQ;
 }
 
-
+/**
+ * @brief 
+ * Displays the process status. 
+ * For now, it displays the text "ps called" 
+ */
 void ps(void)
 {
     putsUart0("ps called\n");
 }
 
+/**
+ * @brief 
+ * Displays the inter-process (thread) communication status
+ */
 void ipcs()
 {
     putsUart0("IPCS called\n");
 }
 
+/**
+ * @brief 
+ * Kills the process (thread) with the matching PID. 
+ * @param pid 
+ */
 void kill(int32_t pid)
 {
   char str[10];
@@ -49,28 +68,54 @@ void pkill(const char name[])
     putsUart0("\n");
 }
 
+/**
+ * @brief
+ * Turns priority inheritance on or off.
+ * @param state
+ */
 void pi(bool state)
 {
     state ? putsUart0("pi on\n") : putsUart0("pi off\n");
     
 }
 
+/**
+ * @brief
+ * Turns preemption on or off.
+ * @param state
+ */
 void preempt(bool state)
 {
     state ? putsUart0("preempt on\n") : putsUart0("preempt off\n");
 }
 
+/**
+ * @brief
+ * Selects scheduling algorithm.
+ * Either priority or round robin.
+ *
+ * @param prio_on
+ */
 void sched(bool prio_on)
 {
     prio_on ? putsUart0("sched prio\n") : putsUart0("sched rr\n");
 }
 
+/**
+ * @brief
+ * Display the PID of the process (thread).
+ * @param name
+ */
 void pidof(const char name[])
 {
     putsUart0(name);
     putsUart0(" launched\n");
 }
 
+/**
+ * @brief
+ * Checks if a process is in the user defined list of processes
+ */
 bool inProcessesList(char list[][10], char name[])
 {
     uint8_t i = 0;
