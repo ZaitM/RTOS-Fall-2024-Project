83088fdd1c4eae0e2950d18455a3c433929d0097
Zait Martinez <zmtzz1411@gmail.com>
2024-11-23 20:54:00 -0600

    Added the logic pidof(), sched(), preempt(). Added the functionality to clear the screen and move the cursor to the top left

--- shell.c
+++ shell.c
@@ -16,6 +16,7 @@
 #include "tm4c123gh6pm.h"
 #include "shell.h"
 #include "CortexM4Registers.h"
+#include "kernel.h"
 
 // REQUIRED: Add header files here for your strings functions, ...
 #include "shell_auxiliary.h"
@@ -32,34 +33,25 @@ void shell(void)
 {
     USER_DATA data;
     data.fieldCount = 0;
-    char proc_name_strs[5][10] = {"proc0", "proc1", "proc2", "proc3", "proc4"};
+
+    // Clear the screen and move the cursor to the top left
+    putsUart0("\033[2J\033[H");
 
     while (true)
     {
         if (kbhitUart0())
         {
             bool foo = 0;
-            // Delete first character in terminal that triggered the kbhitUart0() function
-            putsUart0("\b");
-
-            // Clear the FIFO buffer
-            /*
-             * true if data to be read
-             * false if no data to be read
-             */
-            getcUart0();
-
-            putsUart0("> ");
 
             // Get the string from the user
             getsUart0(&data);
 
             // Echo back to the user of the TTY interface for testing
-
+#ifdef DEBUG
             putsUart0("Output:\n");
             putsUart0(data.buffer);
             putcUart0('\n');
-
+#endif DEBUG
             // Parse fields
             parseFields(&data);
 
@@ -120,52 +112,58 @@ void shell(void)
             }
             else if (isCommand(&data, "preempt", 1))
             {
-                bool state;
-                if (strCmp(getFieldString(&data, 1), "on"))
+                if (strCmp(getFieldString(&data, 1), "ON"))
                 {
-                    state = true;
-                    preempt(state);
+                    putsUart0("Preemptive\n\n");
+                    preempt(PREEMPTIVE);
                     foo = true;
                 }
-                else if (strCmp(getFieldString(&data, 1), "off"))
+                else if (strCmp(getFieldString(&data, 1), "OFF"))
                 {
-                    state = false;
-                    preempt(state);
+                    putsUart0("Cooperative\n\n");
+                    preempt(COOPERATIVE);
                     foo = true;
                 }
             }
             else if (isCommand(&data, "sched", 1))
             {
-                bool state;
-                if (strCmp(getFieldString(&data, 1), "rr"))
+                if (strCmp(getFieldString(&data, 1), "RR"))
                 {
-                    state = false;
-                    sched(state);
+
+                    putsUart0("Round Robin Scheduler\n\n");
+                    sched(ROUND_ROBIN_SCHEDULER);
                     foo = true;
                 }
-                else if (strCmp(getFieldString(&data, 1), "prio"))
+                else if (strCmp(getFieldString(&data, 1), "PRIO"))
                 {
-                    state = true;
-                    sched(state);
+                    putsUart0("Priority Scheduler\n\n");
+                    sched(PRIORITY_SCHEDULER);
                     foo = true;
                 }
             }
             else if (isCommand(&data, "pidof", 1))
             {
-                pidof(getFieldString(&data, 1));
+                uint32_t pid = 0;
+                char str[20] = {0};
+
+                pidof(getFieldString(&data, 1), &pid);
+                if (pid)
+                {
+                    putsUart0("PID: 0x");
+                    itoa(pid, str, 16);
+                    putsUart0(str);
+                    putcUart0('\n');
+                    putcUart0('\n');
+                }
+                else
+                    putsUart0("Process not found\n\n");
+
                 foo = true;
             }
-            if (inProcessesList(proc_name_strs, &data.buffer[data.fieldPosition[0]]))
-                RED_LED = 1;
-            else
-                RED_LED = 0;
-
-            foo ? putsUart0("\nValid command!\n") : putsUart0("\nInvalid command!\n");
-            putsUart0("********************\n");
-
+            if (!foo)
+                putsUart0("Invalid command!\n\n");
             clearStruct(&data);
         }
         yield();
     }
 }
-
